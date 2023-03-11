4c0715862c492ec308e38af3918979300994835b
Eugenia Cáceres <eugeniacaceres.mh@gmail.com>
2023-03-11 20:20:38 -0300

    Agregué incompleto ejercicio 10 falta valores filas y css

---
+++ js/10-ejercicio.js
@@ -0,0 +1,17 @@
+/*10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
+
+Ejercicios con Math*/
+
+let filas = parseInt(prompt("Ingresar N° de filas:"));
+let columnas = parseInt(prompt("Ingresar N° de columnas:"));
+
+document.write("<table> <tbody>");
+
+for (let ifilas = 0; ifilas <= filas; ifilas = ifilas + 1) {
+  document.write("<tr>");
+  for (let icelda = 0; icelda <= columnas; icelda = icelda + 1) {
+    document.write("<td> </td>");
+  }
+  document.write("</tr>");
+}
+document.write("</tbody> </table>");
